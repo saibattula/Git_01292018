d4a6fde452048936427681c90d3e78a92bb6f822
Sanjay_Samala <Sanjay_Samala@HYDPCM145060L.ad.infosys.com>
2018-01-30 10:37:23 +0530

    New UI5 Project

---
+++ GitHub_Demo/WebContent/github_demo/Sample.controller.js
@@ -0,0 +1,46 @@
+sap.ui.controller("github_demo.Sample", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf github_demo.Sample
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf github_demo.Sample
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+	
+	onCreate: function(){
+		alert("Create button pressed")
+	},
+	
+	onDelete: function(){
+		alert("Delete button pressed")
+	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf github_demo.Sample
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf github_demo.Sample
+*/
+//	onExit: function() {
+//
+//	}
+
+});
